a5972427779c4f77e243db6fca5f4f26a903dbf5
Marc <mjlabe@gmail.com>
2018-11-16 14:26:51 -0500

    process and serve file

--- ConsoleApp/views.py
+++ ConsoleApp/views.py
@@ -1,7 +1,11 @@
 import os
 import subprocess
+import time
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+from django.utils.encoding import smart_str
+
 from ConsoleAppIntegration.settings import STATIC_ROOT
 
 
@@ -10,35 +14,34 @@ def simple_upload(request):
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
         uploaded_file_path = fs.path(filename)
-        filename_noext = os.path.splitext(filename)[0]
+        filename_out = "result_" + os.path.splitext(filename)[0] + ".doc"
         filepath = os.path.dirname(uploaded_file_path) + "\\"
 
         app = STATIC_ROOT + "\\ConsoleApplication.exe"
         p = subprocess.Popen([str(app)], stdin=subprocess.PIPE)
 
-        command1 = "1" + "\n"
-        command2 = uploaded_file_path + "\n"
-        command3 = "2" + "\n"
-        command4 = filepath + "result_" + filename_noext + ".doc" + "\n"
-        command5 = "z" + "\n"
+        # Commands to send
+        commands = []
+        commands.append("1" + "\n")
+        commands.append(uploaded_file_path + "\n")
+        commands.append("2" + "\n")
+        commands.append(filepath + filename_out + "\n")
+        commands.append("z" + "\n")
 
         # issue commands...
-        p.stdin.write(command1.encode('utf-8'))
-        p.stdin.write(command2.encode('utf-8'))
-        p.stdin.write(command3.encode('utf-8'))
-        p.stdin.write(command4.encode('utf-8'))
-        p.stdin.write(command5.encode('utf-8'))
+        for command in commands:
+            p.stdin.write(command.encode('utf-8'))
 
         p.stdin.close()
 
-        return render(request, 'simple_upload.html', {
-            'uploaded_file_url': uploaded_file_url
-        })
+        # Wait until process is complete
+        p.communicate()
+
+        # Serve File
+        file_path = open(filepath + filename_out)
+        response = HttpResponse(file_path, content_type='application/force-download')
+        response['Content-Disposition'] = 'attachment; filename=%s' % smart_str(filename_out)
+        return response
+
     return render(request, 'simple_upload.html')
-
-
-def wdir(dir):
-    dir.replace('\\\\', '\\')
-    return dir
